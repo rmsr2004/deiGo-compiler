fae265e8ba7d2fbddd391ffe0aca47b42fe4efbb
rmsr2004 <uc2022233032@student.uc.pt>
2024-12-08 17:45:42 +0000

    update files

--- compile.sh
+++ compile.sh
@@ -21,8 +21,8 @@ mv y.output src/y.output
 mv y.gv src/y.gv
 
 # Compile the lex.yy.c file into the gocompiler binary
-echo -n "cc src/lex.yy.c src/y.tab.c src/ast.c src/symbols.c  -Wall -Wno-unused-function -o src/gocompiler ... "
-clang src/lex.yy.c src/y.tab.c src/ast.c src/symbols.c -Wall -Wno-unused-function -o src/gocompiler -g
+echo -n "clang src/lex.yy.c src/y.tab.c src/ast.c src/symbols.c src/codegen.c -Wall -Wno-unused-function -o src/gocompiler -g ... "
+clang src/lex.yy.c src/y.tab.c src/ast.c src/symbols.c src/codegen.c -Wall -Wno-unused-function -o src/gocompiler -g
 echo "Done!"
 
 # Check if the number of arguments passed is not equal to 1
@@ -34,6 +34,7 @@ fi
 
 # If the argument is '-l', run the gocompiler with the '-l' flag
 if [ "$1" == "-l" ]; then
+    echo "./src/gocompiler -l"
     ./src/gocompiler -l
     rm src/lex.yy.c src/y.tab.c src/y.tab.h src/y.output src/y.gv 
     exit 0
@@ -41,34 +42,52 @@ fi
 
 # If the argument is '-t', run the gocompiler with the '-t' flag
 if [ "$1" == "-t" ]; then
+    echo "./src/gocompiler -t"
+    ./src/gocompiler -t
+    rm src/lex.yy.c src/y.tab.c src/y.tab.h src/y.output src/y.gv 
+    exit 0
+fi
+
+# If the argument is '-s', run the gocompiler with the '-t' flag
+if [ "$1" == "-t" ]; then
+    echo "./src/gocompiler -t"
     ./src/gocompiler -t
     rm src/lex.yy.c src/y.tab.c src/y.tab.h src/y.output src/y.gv 
     exit 0
 fi
 
 # Exit if the argument is not a valid test folder
-folder_name=tests/meta3/${1}
+folder_name=tests/meta4/${1}
 if [ ! -d "$folder_name" ]; then
     echo "File not found: $folder_name"
     rm src/lex.yy.c src/y.tab.c src/y.tab.h src/gocompiler src/y.output src/y.gv 
     exit 1
 fi
 
-# Run the gocompiler with the '-s' flag, using input from the test file and saving output to a file
-echo -n "./src/gocompiler -s < tests/meta3/${1}/${1}.dgo > tests/${1}/output.out ... "
-./src/gocompiler -s < tests/meta3/${1}/${1}.dgo > tests/meta3/${1}/output.out
+# Run the gocompiler without flag to generate code, using input from the test file and saving output to a file
+echo -n "./src/gocompiler < tests/meta4/${1}/${1}.dgo > tests/meta4/${1}/output.out ... "
+./src/gocompiler < tests/meta4/${1}/${1}.dgo > tests/meta4/${1}/${1}.ll
+echo "Done!"
+
+echo -n "llvm-as tests/meta4/${1}/${1}.ll ... "
+llvm-as tests/meta4/${1}/${1}.ll
+echo "Done!"
+
+echo "lli tests/meta4/${1}/${1}.bc > tests/meta4/${1}/output.out ... "
+lli tests/meta4/${1}/${1}.bc > tests/meta4/${1}/output.out
+echo "Done!"
 
 if [[ "${1}" == *"error"* ]]; then
-    sort tests/meta3/${1}/output.out -o tests/meta3/${1}/output.out
+    sort tests/meta4/${1}/output.out -o tests/meta4/${1}/output.out
 fi
 echo "Done!"
 
 # Define the paths for expected and generated output files
-output_expected=tests/meta3/${1}/${1}.out
-output_generated=tests/meta3/${1}/output.out
+output_expected=tests/meta4/${1}/${1}.out
+output_generated=tests/meta4/${1}/output.out
 
 # Compare the expected output with the generated output, showing differences if any
-echo -n "diff tests/meta3/${1}/${1}.out tests/meta3/${1}/output.out ... "
+echo -n "diff tests/meta4/${1}/${1}.out tests/meta4/${1}/output.out ... "
 diff_output=$(diff --side-by-side --suppress-common-lines "$output_expected" "$output_generated" || true)
 echo "Done!"
 echo ""
@@ -84,4 +103,4 @@ else
     echo "$diff_output"
 fi
 
-rm src/lex.yy.c src/y.tab.c src/y.tab.h src/y.output src/y.gv 
+rm src/lex.yy.c src/y.tab.c src/y.tab.h src/y.output src/y.gv tests/meta4/${1}/${1}.bc
